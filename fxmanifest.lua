cdcfc78b17bea8d2a232ba0d303a1ad15a7ea09c
tom2440 <thomasrouviere66@gmail.com>
2025-08-11 16:45:16 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'YourName'
+description 'Helicopter Stabilizer System'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+dependencies {
+    'es_extended'
+}
